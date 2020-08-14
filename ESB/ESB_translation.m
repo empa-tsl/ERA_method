23094a53a67a753248303c9e1cd3a636ef48fc88
hdesing <56823285+hdesing@users.noreply.github.com>
2020-08-14 16:04:02 +0200

    Add files via upload
    Update of boundary values

--- ESB/ESB_translation.m
+++ ESB/ESB_translation.m
@@ -10,7 +10,7 @@ n_runs = 100000;
 ESB = MCrand2(xlsread('ESB_translation.xlsx','PB','D24:G35'),n_runs);
 ESB_distribution = xlsread('ESB_translation.xlsx','PB','G24:G35');
 ESB(8,:,:) = Land_appropriable;
-ESB(12,:,:) = E_total;
+ESB(11,:,:) = E_total .* (3600 * 24 * 365.25 * 10^-6); %Conversion from W-el in MJ/a
 
 save('ESB.mat','ESB','ESB_distribution')
 
